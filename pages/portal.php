05e1766c8cf93552cd0cecd742083a833365594c
kojala <kajsaojala@hotmail.com>
2021-04-23 16:40:56 +0200

    lagt till en css-mapp, skapat portal-filer

---
+++ pages/portal.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+include "../sections/header.php";
+?>
+    <div class="portalWrapper">Portal sidan</div>
+
+<?php 
+include "../sections/footer.php";
+?>
--- sections/header.php
+++ sections/header.php
@@ -2,7 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel='stylesheet' href='/style.css'>
-        <title>CHRONOS</title>
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+        <link rel='stylesheet' href='/css/general.css'>
+        <link rel='stylesheet' href='/css/portal.css'>
+        <title>TELLUS</title>
     </head>
     <body>
