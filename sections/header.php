4875c9d0fe06643a774a9374fa01df9fe2ea58ef
gabbigranqvist <83006746+gabbigranqvist@users.noreply.github.com>
2021-04-27 19:19:54 +0200

    Ryggsäck/inventory

--- sections/header.php
+++ sections/header.php
@@ -7,6 +7,7 @@
         <link rel='stylesheet' href='/css/portal.css'>
         <link rel='stylesheet' href='/css/login.css'>
         <link rel='stylesheet' href='/css/map.css'>
+        <link rel='stylesheet' href='/css/inventory.css'>
         <link rel='stylesheet' href='/css/index.css'>
         <title>TELLUS</title>
     </head>
